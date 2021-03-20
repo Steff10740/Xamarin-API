c62d1afc88ff773a6567b12660c294b1255d2aba
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-19 22:44:20 -0400

    Add files via upload

---
+++ Codigo/SeasonSportAPI/SeasonAPISport/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SeasonAPISport.Views;
+
+namespace SeasonAPISport
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StartPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
