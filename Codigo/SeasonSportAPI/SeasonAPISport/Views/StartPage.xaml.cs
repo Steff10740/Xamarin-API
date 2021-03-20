3a301c1af75c941e7b7dc903978b76d367108c26
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-19 22:43:17 -0400

    Add files via upload

---
+++ Codigo/SeasonSportAPI/SeasonAPISport/Views/StartPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeasonAPISport.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SeasonAPISport.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            BindingContext = new SeasonVM();
+        }
+    }
+}
