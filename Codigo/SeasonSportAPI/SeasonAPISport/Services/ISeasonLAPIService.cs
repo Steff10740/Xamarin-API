db8b29f1b4cdc4036759c04f5293f93db1bd43c3
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-19 22:39:38 -0400

    Add files via upload

---
+++ Codigo/SeasonSportAPI/SeasonAPISport/Services/ISeasonLAPIService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SeasonAPISport.Models;
+
+namespace SeasonAPISport.Services
+{
+    public interface ISeasonLAPIService
+    {
+        Task<SeasonInfo> GetSeasonInfoAsync();
+        
+    }
+}
---
+++ Codigo/SeasonSportAPI/SeasonAPISport/Services/SeasonLAPIService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+using SeasonAPISport.Models;
+using System.Threading.Tasks;
+using SeasonAPISport.Services;
+
+
+namespace SeasonAPISport.Services
+{
+    public class SeasonLAPIService : ISeasonLAPIService
+
+    {
+        public async Task<SeasonInfo> GetSeasonInfoAsync()
+        {
+            SeasonInfo season = null;
+            var client = new HttpClient(); 
+            var request = new HttpRequestMessage 
+            { 
+                Method = HttpMethod.Get, 
+                RequestUri = new Uri("https://sportscore1.p.rapidapi.com/seasons"), 
+                Headers = 
+                { 
+                    { "x-rapidapi-key", "8aaee18d33msh73133a98dbb4908p146d53jsn17633aea599f" }, 
+                    { "x-rapidapi-host", "sportscore1.p.rapidapi.com" }, 
+                }, 
+            };
+            var response = await client.SendAsync(request);
+            if  (response.IsSuccessStatusCode)
+            {
+                season = JsonConvert.DeserializeObject<SeasonInfo>(await response.Content.ReadAsStringAsync());
+                    
+            }
+            return season;
+        }
+    }
+}
