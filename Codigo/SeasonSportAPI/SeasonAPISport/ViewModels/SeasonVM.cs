430baa5386b5393b6d6139b86259a781fe05cca9
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-19 22:41:22 -0400

    Add files via upload

---
+++ Codigo/SeasonSportAPI/SeasonAPISport/ViewModels/SeasonVM.cs
@@ -0,0 +1,41 @@
+﻿using SeasonAPISport.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using static SeasonAPISport.Models.SeasonInfo;
+
+namespace SeasonAPISport.ViewModels
+{
+    public class SeasonVM : INotifyPropertyChanged
+    {
+         ISeasonLAPIService seasonLAIService; 
+        public SeasonVM ()
+        {
+            seasonLAIService = new SeasonLAPIService();
+                GetSeason = new Command(GetSeasonAsync);
+        }
+
+        public async void GetSeasonAsync()
+        {
+            if (!(Connectivity.NetworkAccess == NetworkAccess.Internet))
+            {
+                await App.Current.MainPage.DisplayAlert("Aviso", "Conectese al internet", "OK");
+                return;
+            }
+            IsDataVisible = false;
+            IsBusy = true;
+            var SeasonList = await seasonLAIService.GetSeasonInfoAsync();
+            SeasonslData = SeasonList.Data;
+            IsBusy = false;
+            IsDataVisible = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Datum> SeasonslData { get; set; }
+        public bool IsBusy { get; set; }
+        public bool IsDataVisible { get; set; }
+        public ICommand GetSeason { get; }
+    }
+}
